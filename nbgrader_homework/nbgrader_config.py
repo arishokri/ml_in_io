0285c4a6adb05bdb49c80c407163480b749eab98
Ari Shokri <arashsh0@live.com>
2026-01-20 11:08:49 -0600

    Adds structure for nbgrader; adds source dataset and notebook for hw2; moves hw1 files to the appropriate directory

---
+++ nbgrader_homework/nbgrader_config.py
@@ -0,0 +1,11 @@
+c = get_config()
+
+c.CourseDirectory.course_id = "psyc723"
+
+# Optional but helpful defaults:
+c.Exchange.root = "."
+c.CourseDirectory.course_id = "psyc723"
+c.Exchange.assignment_dir = "release"
+c.Exchange.inbound_directory = "submitted"
+c.Exchange.autograded_directory = "autograded"
+c.Exchange.feedback_directory = "feedback"
